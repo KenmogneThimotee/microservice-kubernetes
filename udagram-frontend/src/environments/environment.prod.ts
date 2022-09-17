ab8e3d98964df131bc013f486c640d9e1ba9bef2
Kenmogne Thimotee <kenmognethimotee@gmail.com>
2022-09-17 14:35:57 +0100

    secret changed

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae625445faedb475e9bf4bd185e4eed4-288804888.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
